7028ffdafd4e333b22e4d7d36e9e590db8433090
Olecram1981 <marcelomoraesmachado@yahoo.com.br>
2022-02-02 21:11:53 -0400

    Projeto Iniciado

---
+++ src/main/java/com/marcelo/sidbebi/SidbebiApplication.java
@@ -0,0 +1,13 @@
+package com.marcelo.sidbebi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidbebiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidbebiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcelo/sidbebi/SidbebiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelo.sidbebi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidbebiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
