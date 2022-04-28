bdcb98d3cc1ec4ae5069156b5a82ab485b57fabe
Olecram1981 <marcelomoraesmachado@yahoo.com.br>
2022-04-28 14:20:20 -0400

    Autorização com JWT

--- src/main/java/com/marcelo/sidbebi/config/SecurityConfig.java
+++ src/main/java/com/marcelo/sidbebi/config/SecurityConfig.java
@@ -18,6 +18,8 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.marcelo.sidbebi.domain.security.JWTAuthenticationFilter;
+import com.marcelo.sidbebi.domain.security.JWTAuthorizationFilter;
+import com.marcelo.sidbebi.domain.security.JWTUtil;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
