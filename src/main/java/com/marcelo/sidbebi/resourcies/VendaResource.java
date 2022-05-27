3029dd330d5a81fbd3ffec4af374b188cd021a48
Olecram1981 <marcelomoraesmachado@yahoo.com.br>
2022-05-27 18:54:28 -0400

    VendaResource atualizado

--- src/main/java/com/marcelo/sidbebi/resourcies/VendaResource.java
+++ src/main/java/com/marcelo/sidbebi/resourcies/VendaResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -55,6 +56,7 @@ public class VendaResource {
 		return ResponseEntity.ok().body(new VendaDTO(obj));
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<VendaDTO> delete(@PathVariable Integer id) {
 		service.delete(id); 
